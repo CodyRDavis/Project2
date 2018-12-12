e3ca416f81e57af2813fa77b36b53646745aa255
Cody Davis <Cody.1337@gmail.com>
2018-12-12 13:30:12 -0600

    initial models created

---
+++ models/user.js
@@ -0,0 +1,59 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.EMAIL,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+    },
+    is_vet: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Pet, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
+
+
+var Post = sequelize.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  },
+  body: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    len: [1]
+  }
+});
