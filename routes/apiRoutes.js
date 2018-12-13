78841374519879d7414e19092e5941b67ef6322a
CodyRDavis <39285068+CodyRDavis@users.noreply.github.com>
2018-12-13 10:17:31 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,36 +6,36 @@ module.exports = function(app) {
   //USER ROUTES
   //===========================================
   // Get all examples
-  app.get("/API/user", function(req, res) {
+  app.get("/api/user", function(req, res) {
     res.send("API USER GET");
   });
 
   // Create a new user
-  app.post("/API/user", function(req, res) {
+  app.post("/api/user", function(req, res) {
     res.send("API USER POST");
   });
   // Update an user by id
-  app.put("/API/user/:id", function(req, res){
+  app.put("/api/user/:id", function(req, res){
     res.send("API USER PUT");
   });
   // Delete an user by id
-  app.delete("/API/user/:id", function(req, res) {
+  app.delete("/api/user/:id", function(req, res) {
     res.send("API USER DELETE");
   });
 
 
   //PET ROUTES
   //=============================================
-  app.get("/API/pet", function(req, res){
+  app.get("/api/pet", function(req, res){
     res.send("API PET GET");
   });
-  app.post("/API/pet", function(req,res){
+  app.post("/api/pet", function(req,res){
     res.send("API PET POST");
   });
-  app.put("/API/pet/:id", function (req,res){
+  app.put("/api/pet/:id", function (req,res){
     res.send("API PET PUT");
   });
-  app.delete("/API/pet/:id", function(req,res){
+  app.delete("/api/pet/:id", function(req,res){
     res.send("AP PET DELETE");
   });
 };
