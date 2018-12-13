47f02192593b64232998612da46a9224e3f51c41
Cody Davis <codydavis@codys-imac.lan>
2018-12-12 19:04:47 -0600

    basic crud framework for pets and users

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
+require("./routes/userApiRoutes")(app);
+require("./routes/petApiRoutes")(app);
+//require("./routes/taskApiRoutes")(app);//not sure what to do with this one yet or how to name it.
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
