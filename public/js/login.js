2486dd86cf70fe763d15ffa67c2273d846f93cd4
Cody Davis <Cody.1337@gmail.com>
2018-12-14 14:01:09 -0600

    fixed lint issues

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,7 @@ $("document").ready(function(){
       email: $("#email").val(),
       password: $("#password").val(),
     }).then(function(result){
+      console.log(result);
     });
   });
 });
