7e7402534bff3d37f1cf8f11594670af4fcb476a
Cody Davis <Cody.1337@gmail.com>
2018-12-18 12:31:37 -0600

    ability to draw card from ajax added

--- public/js/ownerDashboard.js
+++ public/js/ownerDashboard.js
@@ -3,8 +3,32 @@ $("document").ready(function(){
   $.get("/api/user").then(function(result){
     console.log(result);
     for (let i = 0; i < result.data.Pets.length; i++){
+
       let pet = result.data.Pets[i];
-      console.log ("Welcome Back Mr." + pet.name);
+
+      let card = $("<div>").addClass("card");
+      let cardHeader = $("<div>").addClass("card-header").append("<h3>"+pet.name+"</h3>");
+      let cardBody = $("<div>").addClass("card-body");
+      cardBody.append("<h5>Species: "+ petInfoValidate(pet.species)+"</h5>");
+      cardBody.append("<h5>Breed: "+ petInfoValidate(pet.breed) + "</h5>");
+      cardBody.append("<h5>Age: "+ petInfoValidate(pet.age) + "</h5>");
+      cardBody.append("<h5>Chip Number: "+ petInfoValidate(pet.chipNumber) + "</h5>");
+      cardBody.append("<h5>Allergies: "+ petInfoValidate(pet.allergies) + "</h5>");
+      cardBody.append("<h5>Notes: "+ petInfoValidate(pet.notes) + "</h5>");
+
+
+      //card assembly
+      card.append(cardHeader, cardBody);
+      $("#container-empty").append(card);
+
     }
   });
 });
+
+function petInfoValidate(info){
+  if (!info){
+    return ("Not Specified");
+  } else {
+    return info;
+  }
+}
