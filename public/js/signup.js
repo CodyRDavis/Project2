c33dbf1d9040f8f3976bb3fe976188486df6cd47
Cody Davis <Cody.1337@gmail.com>
2018-12-18 13:38:14 -0600

    sign up validation of user input

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,38 @@
 $("document").ready(function(){
   $("#add-btn").click(function(e){
-    e.preventDefault();
-    $.post("/api/signup", {
-      name: $("#name").val(),
-      email: $("#email").val().toLowerCase(),
-      password: $("#password").val(),
-      phoneNumber: 0,
-      is_vet: false
-    }).then(function(result){
-      console.log("submitted");
-      console.log(result);
 
-      //checking to see if creating a user failed or not.
-      if (result.success === false){
-        //ANNIE NGO MODLE HERE!
-        alert(result.error);
-      } else {
-        console.log ("navigate to " + result.data.location);
-        window.location.replace(result.data.location);
-      }
-    });
+    e.preventDefault();
+
+    let userName = $("#name").val().trim();
+    let userEmail = $("#email").val().toLowerCase().trim();
+    let userPassword = $("#password").val().trim();
+    if(!userName){
+      alert("Please fill the name field!");
+    } else if (!userEmail){
+      alert("Please provide an email address!");
+    } else if (!userPassword){
+      alert("Please provide a password");
+    } else{
+      $.post("/api/signup", {
+        name: $("#name").val(),
+        email: $("#email").val().toLowerCase(),
+        password: $("#password").val(),
+        phoneNumber: 0,
+        is_vet: false
+      }).then(function(result){
+        console.log("submitted");
+        console.log(result);
+
+        //checking to see if creating a user failed or not.
+        if (!result.success){
+          //ANNIE NGO MODLE HERE!
+          alert(result.error);
+        } else {
+          console.log ("navigate to " + result.data.location);
+          window.location.replace(result.data.location);
+        }
+      });
+    }
+    
   });
 });
