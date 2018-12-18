1742a7a1159f651c9b3660b92df918da554d11cb
Cody Davis <Cody.1337@gmail.com>
2018-12-18 10:44:54 -0600

    dash now looping for each pet incoming

--- public/js/ownerDashboard.js
+++ public/js/ownerDashboard.js
@@ -1,6 +1,10 @@
 $("document").ready(function(){
   console.log("page ready");
-  $.get("/api/user").then(function(data){
-    console.log(data);
+  $.get("/api/user").then(function(result){
+    console.log(result);
+    for (let i = 0; i < result.data.Pets.length; i++){
+      let pet = result.data.Pets[i];
+      console.log ("Welcome Back Mr." + pet.name);
+    }
   });
 });
